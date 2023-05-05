d84a193998986e946f021af5f5b62b0d13ea86d1
jakobmerljak <merljak.jakob@gmail.com>
2023-05-05 14:35:29 +0200

    Switch to setup.cfg

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    name = 'aCT-client',
-    version = '0.1',
-    url = 'http://github.com/ATLASControlTower/aCT',
-    author = 'aCT team',
-    author_email = 'act-dev@cern.ch',
-    package_dir = {'': 'src'},
-    packages=find_packages('src'),
-    entry_points={
-        'console_scripts': [
-            'act = act_client.cli:main',
-        ]
-    },
-    install_requires=[
-        'cryptography',
-        'pyyaml',
-        'lark',
-    ]
-)
+setup()
